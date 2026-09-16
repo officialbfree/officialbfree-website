f590f3e2956d7b9486cfe1c73fcf3136dbc2b0fe
officialbfree <officialbfree@gmail.com>
2026-09-16 19:57:53 -0400

    Revert hero video fully to last known-working config

--- script.js
+++ script.js
@@ -421,7 +421,7 @@ var SITE_CONTENT = null;
     if (!filled(id)) return;
 
     var origin = window.location.origin;
-    var src = "https://www.youtube.com/embed/" + id +
+    var src = "https://www.youtube-nocookie.com/embed/" + id +
       "?autoplay=1&mute=1&loop=1&playlist=" + id +
       "&controls=0&modestbranding=1&playsinline=1&rel=0&iv_load_policy=3" +
       "&enablejsapi=1&origin=" + encodeURIComponent(origin);
